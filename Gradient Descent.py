8d4c46d35296b35eaee0a7fb199aed6f28b65e41
edwinroca <56678036+edwinroca@users.noreply.github.com>
2020-06-15 14:44:32 +0100

    update plotting

--- Gradient Descent.py
+++ Gradient Descent.py
@@ -1,29 +1,33 @@
 # Gradient decent implementation
-#%%
+
 import csv
+import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt  
+
 data = np.genfromtxt("student_scores.csv", delimiter=",", skip_header=1)	
 X = data[:,0]
 Y = data[:,1]
 length = data.shape[0]
-#%%
-m = 5 # initialising the slope
-c = 10 # initialising the intercept
-learning_rate = 0.004 # try 0.001 to 0.04, 0.05 , 0.06, 0.07, 0.08 
+
+m = 0 # initialising the slope
+c = 20 # initialising the intercept
+b = c # to set intercept axis
+learning_rate = 0.005 # try 0.001 to 0.04, 0.05 , 0.06, 0.07, 0.08 
 # (0.07 produces oscilation)
-delta_slope = []
-delta_intercept = []
+slope = []
+slope_change=[]
+intercept = []
 cost_f = []
+iteration = []
+fig, axs = plt.subplots(2,2,figsize=(10,8))
+N = 150
 
-plt.figure(1)
-plt.plot(X,Y,'o')
-plt.title("Implementing Gradient Descent")
-plt.xlabel("Hours Studied")
-plt.ylabel("Student Score")
-
-for i in range(0,100):
+delay = 1
+for i in range(0,N):
+	iteration.append(i)
+	print(i)
     # gradient descent
 	sum_error = 0
 	for point in data:
@@ -41,38 +45,50 @@ for i in range(0,100):
 		sum_error += error*error # total squared error per iteration
 	# The goal of any Machine Learning Algorithm 
 	# is to minimize the cost Function
+	# cost_f = (sum_error/length)
 	cost_f.append(sum_error/length) # storing result of cost function per iteration
 	# check for a change on the slope and intercept change with the number of iterations
 	# to see if it converges to a value
-	delta_slope.append(delta_m)
-	delta_intercept.append(delta_b)
+	slope.append(m)
+	slope_change.append(delta_m)
+	intercept.append(c)
 	# Prediction for each iteration
 	regression_y = []
 	for x in X:
 		y = m*x + c
 		regression_y.append(y)
 
-plt.figure(1)
-plt.plot(X,Y,'o')
-plt.plot(X,regression_y)
-# Plotting the cost function
-plt.figure(2)
-plt.plot(cost_f)
-plt.title("Error")
-plt.xlabel("Iteration")
-plt.ylabel("Error value")	
-# plotting the slope change
-plt.figure(3)
-plt.plot(delta_slope)
-plt.title("Slope")
-plt.xlabel("Iteration")
-plt.ylabel("Slope change value")	
-# plotting the intercept change
-plt.figure(4)
-plt.plot(delta_intercept)
-plt.title("Intercept")
-plt.xlabel("Iteration")
-plt.ylabel("Intercept change value")	
-plt.show	
+	axs[0,0].cla()
+	axs[0,0].plot(X,regression_y)
+	axs[0,0].plot(X,Y,'o')
+	axs[0,0].set_xlabel("Hours Studied")
+	axs[0,0].set_ylabel("Student Score & y=m*x+c")
+	axs[0,0].set_ylim((0,100))
+	axs[0,0].set_xlim((0,11))
 
-# %%
+	axs[0,1].cla()
+	axs[0,1].plot(cost_f)
+	axs[0,1].set_ylabel("Error value")
+	axs[0,1].set_xlabel("Iteration")
+	axs[0,1].set_ylim((0,1000))
+	axs[0,1].set_xlim((0,N))
+
+	axs[1,0].cla()
+	axs[1,0].plot(slope,'tab:orange')
+	axs[1,0].set_xlabel("Iteration")
+	axs[1,0].set_ylabel("Slope")
+	axs[1,0].set_xlim((-5,N))
+
+	axs[1,1].cla()
+	axs[1,1].plot(intercept,'tab:green')
+	axs[1,1].set_xlabel("Iteration")
+	axs[1,1].set_ylabel("Intercept")
+	axs[1,1].set_ylim((-b,b))
+	axs[1,1].set_xlim((0,N))
+	delay = delay*0.5
+	plt.pause(delay)
+print('error:', sum_error/length)
+print('Slope change:', delta_m)
+print('Slope:', m)
+print('intercept:',c)
+plt.show()
